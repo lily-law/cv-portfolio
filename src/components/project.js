f6288116761334ef772d93afe2bbfc08385f26df
Lily <46561378+lily-law@users.noreply.github.com>
2020-06-19 19:27:56 +0100

    Move deploy/repo links from Project into feature

--- src/components/project.js
+++ src/components/project.js
@@ -1,16 +1,12 @@
 import Icon from './icon'
 import React, { Fragment } from 'react'
 
-export default function Project({title, link, repo, icons, description, index, feature, setFeature}) {
+export default function Project({title, icons, description, index, feature, setFeature}) {
     return (<Fragment>
         <button className={feature === index ? 'active' : ''} onClick={feature === index ? () => setFeature(null) : () => setFeature(index)}>
             <header>
                 <h1>{title}</h1>
             </header>
-            <nav className="links">
-                {link && <Icon name="deploy" link={link} />}
-                {repo && <Icon name="repo" link={repo} />}
-            </nav>
             <div className="description">
                 <p>{description}</p>
                 {feature === index ?
@@ -33,9 +29,9 @@ export default function Project({title, link, repo, icons, description, index, f
                 width: 100%;
                 display: grid;
                 grid-template-areas: 
-                "links header"
-                "icons description"
-                "icons description";
+                "header icons"
+                "description icons"
+                "description icons";
                 margin-right: 24px;
                 place-items: center center;
                 grid-template-columns: 1fr 1fr;
@@ -74,18 +70,10 @@ export default function Project({title, link, repo, icons, description, index, f
                 grid-area: description;
                 font-weight: 600;
             }
-            .links {
-                grid-area: links;
-                display: inline-flex;
-                flex-flow: row wrap;
-                padding-bottom: 8px;
-                border-bottom: 1px solid var(--grey-l);
-            }
             @media only screen and (max-width: 600px) { 
                 article {
                     grid-template-areas: 
                     "header header"
-                    "links links"
                     "icons icons"
                     "description description";
                     place-items: center end;
