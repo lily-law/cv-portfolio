4ab8a24a56f5063f6e6c4577a03a4393b130e3f5
lily-law <lilylaw.dev@gmail.com>
2020-09-04 14:22:50 +0100

    Update CV text

--- src/components/cv.js
+++ src/components/cv.js
@@ -16,51 +16,92 @@ const Cv = () => {
                 <article>
                     <figure>
                         <figcaption><Icon name="lilylaw" /></figcaption>
+                        <h2>Frontend Web Developer</h2>
                     </figure> 
                     <section>
                         <p>I began learning to code with some downtime after having surgery and I've been hooked since.</p>
+                        <p>My ethos is to stay fairly tech stack agnostic and focus on the language and best programming practices.</p>
+                        <p>Often quick to learn and adapt, I enjoy the challenge and constant need to shift perspective when learning new tech</p>
                     </section>
-                    <section>
+                    <section className="experience">
                         <header>
-                            <h2>Strengths at every stage of development.</h2>
+                           <h3>Experience</h3>
                         </header>
                         <ul>
-                            <li>Artistic with a User Focus</li>
-                            <li>Empathetic and Introspective</li>
-                            <li>Curious and Persistent</li>
+                            <li>
+                                <h4>Facilitator at Chingu.io</h4>
+                                <h5>04/2020 - present</h5>
+                                <p>
+                                    In this role I evaluate solo project submissions. I've also played a part in planning, 
+                                    designing, and developing projects for fellow Chingus to base their solo projects on.
+                                </p>
+                            </li>
+                            <li>
+                                <h4>Chingu Voyager</h4>
+                                <h5>01/2020 - present</h5>
+                                <p>
+                                    I've worked in several teams so far on these 6 week agile developer projects. 
+                                    Each time I've gained a treasure trove of insight about my strengths and weaknesses.
+                                    One of the most valuable lessons I've learned from them is how integral planning is.
+                                </p>
+                            </li>
+                            <li>
+                                <h4>Freelance Web Designer and Developer</h4>
+                                <h5>01/2019 - 12/2019</h5>
+                                <p>
+                                    This role taught me a ton about real world development. 
+                                    It set me on the path to building the self discipline I have now and has reassured me I'm headed in the right direction.
+                                </p>
+                            </li>
                         </ul>
                     </section>
                 </article>
                 <aside>
                     <section>
                         <header>
-                            <h2>Proficient Frontend Developer</h2>
+                            <h3>Skills</h3>
                         </header>
                         <figure>
                             <Icon name="js" className="big" />
                         </figure>
                         <div className="icons">
-                            <Icon name="css" />
-                            <Icon name="html5" />
                             <Icon name="react" />
                             <Icon name="xd" />
+                            <Icon name="css" />
+                            <Icon name="html5" />
                         </div>   
+                        <ul>
+                            <li>JavaScript</li>
+                            <li>React</li>
+                            <li>Adobe XD</li>
+                            <li>CSS</li>
+                            <li>HTML</li>
+                            <li>git</li>
+                        </ul>
                     </section>
                     <section>
-                        <header>
-                            <h2>Progressing in Backend Development</h2>
-                        </header>
-                        <figure>
-                            <Icon name="nodejs" className="big" />
-                        </figure>
-                    </section>
-                    <section>
-                        <h3>Experienced working in remote teams</h3>
+                        <h4>Experienced working in remote teams</h4>
                         <div className="icons">
                             <Icon name="agile" />
                             <Icon name="chingu" />
                             <Icon name="github" />
                         </div>
+                        <ul>
+                            <li>Agile</li>
+                            <li>Chingu</li>
+                            <li>GitHub</li>
+                        </ul>
+                    </section>
+                    <section>
+                        <h4>Strengths at every stage of development</h4>
+                        <ul>
+                            <li>Motivated team player</li>
+                            <li>Independent thinker</li>
+                            <li>Artistic with a User Focus</li>
+                            <li>Able to lead</li>
+                            <li>Empathetic and Introspective</li>
+                            <li>Curious and Persistent</li>
+                        </ul>
                     </section>
                 </aside>
             </main>
