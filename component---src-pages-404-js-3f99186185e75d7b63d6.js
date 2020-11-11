6e20343b0b8110ee44d275b9028b9f3cf69553f7
lily-law <lilylaw.dev@gmail.com>
2020-11-11 13:35:30 +0000

    Updates

--- component---src-pages-404-js-3f99186185e75d7b63d6.js
+++ component---src-pages-404-js-3f99186185e75d7b63d6.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("vrFN");t.default=function(){return l.a.createElement("main",null,l.a.createElement(u.a,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-5a89242ad63b70cabf77.js.map
+//# sourceMappingURL=component---src-pages-404-js-3f99186185e75d7b63d6.js.map
